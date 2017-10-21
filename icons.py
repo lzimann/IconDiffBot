e06556f11606f4c3f92ca780507ab6022f92c56a
LeoZ <leoztgstation@gmail.com>
2017-10-21 10:25:02 -0200

    Initial Commit

---
+++ icons.py
@@ -0,0 +1,147 @@
+import os
+import re
+
+from collections import OrderedDict
+from PIL import Image, ImageChops
+
+import numpy as np
+
+def parse_metadata(img):
+    """Parses a DMI metadata, returning an OrderedDict. img is a PIL.Image object"""
+    dict = img.info
+    info = dict['Description']
+    info = info.split('\n')
+    if not 'version = 4.0' in info:
+        return
+    dict = OrderedDict()
+    current_key = None
+    for entry in info:
+        if not '\t' in entry:
+            current_key = entry
+            dict.update({current_key : []})
+            continue
+        dict[current_key].append(entry.replace('\t', ''))
+    return dict
+
+def generate_icon_states(filename):
+    """Generates every icon state into an Image object. Returning a dict with {name : object}"""
+    frame_dir_re = re.compile('(dirs|frames) = (\d+)')
+    img = Image.open(filename)
+    data_type = np.dtype('int')
+
+    meta_data = parse_metadata(img)
+    try:
+        sizes = meta_data['version = 4.0']
+    except KeyError:
+        print("DMI version not supported!")
+        return
+
+    image_width = img.width
+    image_height = img.height
+
+    icon_width = int(re.search('width = (\d+)', sizes[0]).group(1))
+    icon_height = int(re.search('height = (\d+)', sizes[1]).group(1))
+    img_data = img.getdata()
+
+    total_size = image_width * image_height
+    icons_per_line = int(image_width / icon_width)
+    total_lines = int(image_height / icon_height)
+    if img.mode != "RGBA":
+        img = img.convert("RGBA")
+    data = np.asarray(img)
+    
+    img.close()
+
+    #Cut the first two and the last two elements of the list
+    meta_data.popitem() # Pop last
+    meta_data.popitem() # Pop last again
+    meta_data.popitem(False) # Pop first
+    meta_data.popitem(False) # Pop first again
+
+    total_icons = len(meta_data)
+
+    icon_names = []
+    for key in meta_data.items():
+        icon_names.append(key)
+    icon_count = 0
+    skip_naming = 0
+    name_count = 1
+    icons = {}
+    for line in range(0, total_lines):
+        icon = 0
+        while icon < icons_per_line:
+            this_icon = Image.new('RGBA', (icon_width, icon_height))
+            try:
+                this_icon_dict = icon_names[icon_count] # name : [dirs, frames, delay]
+                name = this_icon_dict[0].replace('state = ', '').replace('\"', '')
+                if skip_naming:
+                    if name_count > 0:
+                        name += str(name_count)
+                    name_count += 1
+                    skip_naming -= 1
+                    if not skip_naming:
+                        icon_count += 1
+                else:
+                    for item in this_icon_dict[1]:
+                        match = frame_dir_re.search(item)
+                        if match:
+                            skip_naming += (int(match.group(2)) - 1)
+                    if not skip_naming:
+                        icon_count += 1
+                    else:
+                        name_count = 1
+            except IndexError:
+                break #IndexError means blank icon
+            icon_start_w = icon * icon_width
+            icon_end_w = icon_start_w + icon_width
+            icon_start_h = line * icon_height
+            icon_end_h = icon_start_h + icon_height
+            x = 0
+            for i in range(icon_start_w, icon_end_w):
+                y = 0
+                for j in range(icon_start_h, icon_end_h):
+                    this_icon.putpixel((x, y), tuple(data[j,i]))
+                    y += 1
+                x += 1
+
+            icon += 1
+            icons[name] = this_icon
+    return icons
+
+
+def check_icon_state_diff(image_a, image_b):
+    """Compares two icons(passed as an Image object), returning True if the icons are equal, False in case of a difference."""
+    return ImageChops.difference(image_a, image_b).getbbox() is None
+
+
+def compare_two_icon_files(file_a, file_b):
+    """Compares every icon state of two icons, returning a dict with the icon state status: {state name : {status : no_check/modified/created, img_a : Image obj, img_b : Image obj}})"""
+    if file_a:
+        file_a_dict = generate_icon_states(file_a)
+    else:
+        file_a_dict = {}
+    file_b_dict = generate_icon_states(file_b)
+    final_dict = {}
+    for key in file_a_dict:
+        final_dict[key] = {}
+        if not file_b_dict.get(key) or check_icon_state_diff(file_a_dict[key], file_b_dict[key]):
+            final_dict[key]["status"] = "no_check"
+            file_a_dict[key].close()
+            try:
+                file_b_dict[key].close() # Since they are equal, close both images
+            except KeyError:
+                continue
+        elif not check_icon_state_diff(file_a_dict[key], file_b_dict[key]):
+            final_dict[key]["status"] = "Modified"
+            final_dict[key]["img_a"] = file_a_dict[key]
+            final_dict[key]["img_b"] = file_b_dict[key]
+
+    for key in file_b_dict:
+        if not file_a_dict.get(key):
+            final_dict[key] = {"Status" : "Created", "img_b" : file_b_dict[key]}
+
+    return final_dict
+
+if __name__ == '__main__':
+    with open("cables.dmi", 'rb') as f:
+        generate_icon_states(f)
---
+++ pr_icon_differ.py
@@ -0,0 +1,170 @@
+import os
+import re
+import hmac
+import json
+from hashlib import sha1
+import requests
+from twisted.web import resource, server
+from twisted.internet import reactor, endpoints
+
+import icons
+
+config = {}
+if os.path.exists(os.path.abspath('config.json')):
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+else:
+    print("Make sure the config file exists.")
+
+github_secret = config['github']['secret'].encode('utf-8')
+github_user = config['github']['user']
+github_auth = config['github']['auth']
+actions_to_check = ['opened', 'synchronize']
+binary_regex = re.compile('diff --git a\/(.*\.dmi) b\/.?')
+
+upload_api_url = config['upload_api']['url']
+upload_api_key = config['upload_api']['key']
+
+def compare_secret(secret_to_compare, payload):
+    """fuck you"""
+    if secret_to_compare is None:
+        return False
+   
+    this_secret = hmac.new(github_secret, payload, sha1)
+    secret_to_compare = secret_to_compare.replace('sha1=', '')
+    return hmac.compare_digest(secret_to_compare, this_secret.hexdigest())
+
+def check_diff(diff_url):
+    """checks the diff url for icons"""
+    req = requests.get(diff_url)
+    if req.status_code == 404:
+        return None
+    diff = req.text.split('\n')
+    icons_with_diff = []
+    for line in diff:
+        match = binary_regex.search(line)
+        if not match:
+            continue
+        icons_with_diff.append(match.group(1))
+    return icons_with_diff
+
+def check_icons(icons_with_diff, base, head, issue_url, send_message = True):
+    if not os.path.exists('./icon_dump'):
+        os.makedirs('./icon_dump')
+    icon_path_a = './icon_dump/old.dmi'
+    icon_path_b = './icon_dump/new.dmi'
+    base_repo_url = base.get('repo').get('html_url')
+    head_repo_url = head.get('repo').get('html_url')
+    msgs = ["Icons with diff:"]
+    for icon in icons_with_diff:
+        response_a = requests.get('{}/blob/{}/{}'.format(base_repo_url, base.get('ref'), icon), data={'raw':  1})
+        response_b = requests.get('{}/blob/{}/{}'.format(head_repo_url, head.get('ref'), icon), data={'raw':  1})
+        if response_a.status_code == 200:
+            with open(icon_path_a, 'wb') as f:
+                f.write(response_a.content)
+        elif response_a.status_code == 404:
+            icon_path_a = None
+        if response_b.status_code == 200:
+            with open(icon_path_b, 'wb') as f:
+                f.write(response_b.content)
+        elif response_b.status_code == 404: #This means the file is being deleted, which does not interest us
+            try:
+                os.remove(icon_path_a)
+                continue
+            except OSError:
+                continue
+        this_dict = icons.compare_two_icon_files(icon_path_a, icon_path_b)
+        if not this_dict:
+            continue
+        msg = ["<details><summary>{}</summary>\n".format(icon), "Key | Old | New | Status", "--- | --- | --- | ---"]
+        for key in this_dict:
+            status = this_dict[key].get("status")
+            if status == 'no_check':
+                continue
+            path_a = './icon_dump/old_{}.png'.format(key)
+            path_b = './icon_dump/new_{}.png'.format(key)
+            img_a = this_dict[key].get('img_a')
+            if img_a:
+                img_a.save(path_a)
+                with open(path_a, 'rb') as f:
+                    url_a = "![{}]({})".format(key, requests.post(upload_api_url, data={'key' : upload_api_key}, files={'file' : f}).json().get('url'))
+            else:
+                url_a = "![]()"
+            img_b = this_dict[key].get('img_b')
+            if img_b:
+                img_b.save(path_b)
+                with open(path_b, 'rb') as f:
+                    url_b = "![{}]({})".format(key, requests.post(upload_api_url, data={'key' : upload_api_key}, files={'file' : f}).json().get('url'))
+            else:
+                url_b = "![]()"
+            if os.path.exists(path_a):
+                os.remove(path_a)
+            if os.path.exists(path_b):
+                os.remove(path_b)
+
+            msg.append("{key}|{url_a}|{url_b}|{status}".format(key=key, url_a=url_a, url_b=url_b, status=status))
+        msg.append("</details>")
+        msgs.append("\n".join(msg))
+    if send_message:
+        github_api_url = "{issue}/comments".format(issue=issue_url)
+        if len(msgs) > 1:
+            body = json.dumps({'body' : '\n'.join(msgs)})
+            requests.post(github_api_url, data=body, auth=(github_user, github_auth))
+    if os.path.exists(icon_path_a):
+        os.remove(icon_path_a)
+    if os.path.exists(icon_path_b):
+        os.remove(icon_path_b)
+
+class Handler(resource.Resource):
+    isLeaf = True
+    def render_POST(self, request):
+        print("POST received.")
+        payload = request.content.getvalue()
+        if not compare_secret(request.getHeader('X-Hub-Signature'), payload):
+            request.setResponseCode(401)
+            return b"Secret does not match"
+        if request.getHeader('X-GitHub-Event') != 'pull_request':
+            request.setResponseCode(404)
+            return b"Event not supported"
+
+        #Then we check the PR for icon diffs
+        payload = json.loads("".join(map(chr, payload)))
+        request.setResponseCode(200)
+        pr_obj = payload['pull_request']
+        if payload['action'] not in actions_to_check:
+            return b"Not actionable"
+        issue_url = pr_obj['issue_url']
+        pr_diff_url = pr_obj['diff_url']
+        head = pr_obj['head']
+        base = pr_obj['base']
+        if payload['action'] == 'synchronize':
+            pr_diff_url = "{html_url}/commits/{sha}.diff".format(html_url=pr_obj['html_url'], sha=head['sha'])
+        icons_with_diff = check_diff(pr_diff_url)
+        if icons_with_diff:
+            print("Diff on PR {}!".format(payload['number']))
+            check_icons(icons_with_diff, base, head, issue_url)
+        return b"Ok"
+    def render_GET(self, request):
+        request.setResponseCode(666)
+        return b"Fuck u"
+
+def test_pr(number, send_message = False):
+    """tests a pr for the icon diff"""
+    req = requests.get("https://api.github.com/repos/tgstation/tgstation/pulls/{}".format(number))
+    if req.status_code == 404:
+        print("PR does not exist")
+        return
+    payload = req.json()
+    icons = check_diff(payload['diff_url'])
+    print("Icons:")
+    print("\n".join(icons))
+    check_icons(icons, payload['base'], payload['head'], payload['html_url'], send_message)
+
+if __name__ == '__main__':
+    endpoints.serverFromString(reactor, "tcp:{}".format(config['webhook_port'])).listen(server.Site(Handler()))
+    try:
+        print("Listening for requests on port: {}.".format(config['webhook_port']))
+        test_pr("31972", True)
+        #reactor.run()
+    except KeyboardInterrupt:
+        pass
